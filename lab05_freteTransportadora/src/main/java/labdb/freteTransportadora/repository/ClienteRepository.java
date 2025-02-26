22e1b901effd896aa344f35328c06b158db37a5b
lluizaslm <marialuizalima577@gmail.com>
2025-02-25 21:19:40 -0300

    -- add ClienteRepository

--- lab05_freteTransportadora/src/main/java/labdb/freteTransportadora/repository/ClienteRepository.java
+++ lab05_freteTransportadora/src/main/java/labdb/freteTransportadora/repository/ClienteRepository.java
@@ -1,12 +1,33 @@
 package labdb.freteTransportadora.repository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
 import labdb.freteTransportadora.models.Cliente;
 import java.util.List;
 
 public class ClienteRepository {
+    private final DAOGenerico<Cliente> dao;
 
+    public ClienteRepository(EntityManager entityManager) {
+        this.dao = new DAOGenerico<>(entityManager);
+    }
+
+    public List<Cliente> buscarTodos() {
+        return dao.buscarTodos(Cliente.class);
+    }
+
+    public Cliente buscaPorId(Integer id) {
+        return dao.buscaPorId(Cliente.class, id);
+    }
+
+    public Cliente salvar(Cliente cliente) {
+        return dao.salvar(cliente);
+    }
+
+    public Cliente atualizar(Cliente cliente) {
+        return dao.atualizar(cliente);
+    }
+
+    public void remover(Cliente cliente) {
+        dao.remove(cliente);
+    }
 }
-
-
